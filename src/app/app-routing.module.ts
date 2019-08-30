a7aa5f6e5207d91c2d9f988eafb58ad22d4e1265
Thiago Rodrigues Alves <alvesthiago92@gmail.com>
2019-08-30 00:16:20 -0300

    Criação de rotas para lazyload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,17 +10,43 @@ const routes: Routes = [
   },
   {
     path: 'people',
-    loadChildren: () =>
-      {
-        return import('./core/people/people.module').then(m => m.PeopleModule);
-      }
+    loadChildren: () => {
+      return import('./core/people/people.module').then(m => m.PeopleModule);
+    }
   },
   {
     path: 'films',
-    loadChildren: () =>
-      {
-        return import('./core/films/films.module').then(m => m.FilmsModule);
-      }
+    loadChildren: () => {
+      return import('./core/films/films.module').then(m => m.FilmsModule);
+    }
+  },
+  {
+    path: 'planets',
+    loadChildren: () => {
+      return import('./core/planets/planets.module').then(m => m.PlanetsModule);
+    }
+  },
+  {
+    path: 'species',
+    loadChildren: () => {
+      return import('./core/species/species.module').then(m => m.SpeciesModule);
+    }
+  },
+  {
+    path: 'vehicles',
+    loadChildren: () => {
+      return import('./core/vehicles/vehicles.module').then(
+        m => m.VehiclesModule
+      );
+    }
+  },
+  {
+    path: 'starships',
+    loadChildren: () => {
+      return import('./core/starships/starships.module').then(
+        m => m.StarshipsModule
+      );
+    }
   }
 ];
 
